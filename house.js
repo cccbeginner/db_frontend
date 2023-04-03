f4869fe27226ada93d0f2e2224f46d93d37c4408
cccbeginner <arvin.tsai.0113@gmail.com>
2023-04-03 10:34:46 +0800

    add test variables & available house product links

---
+++ house.js
@@ -0,0 +1,10 @@
+class House{
+    constructor(url, imgUrl, title, price){
+        this.url = url;
+        this.imgUrl = imgUrl;
+        this.title = title;
+        this.price = price
+    }
+}
+
+module.exports = House;
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const House = require('./house.js')
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
 
+test_houses=[
+  new House('https://www.dd-room.com/','https://dummyimage.com/450x300/dee2e6/6c757d.jpg', 'House title 1', '5000'),
+  new House('https://www.dd-room.com/','https://dummyimage.com/450x300/dee2e6/6c757d.jpg', 'House title 2', '9000'),
+  new House('https://www.dd-room.com/','https://dummyimage.com/450x300/dee2e6/6c757d.jpg', 'House title 3', '7000'),
+  new House('https://www.dd-room.com/','https://dummyimage.com/450x300/dee2e6/6c757d.jpg', 'House title 4', '6000'),
+  new House('https://www.dd-room.com/','https://dummyimage.com/450x300/dee2e6/6c757d.jpg', 'House title 5', '6500'),
+  new House('https://www.dd-room.com/','https://dummyimage.com/450x300/dee2e6/6c757d.jpg', 'House title 6', '5500')
+]
+
 app.get('/', (req, res) => {
-  res.render('index', { title: 'My Node Project' });
+  res.render('index', { title: 'My Node Project' , houses: test_houses});
 });
 
 app.listen(port, () => {
